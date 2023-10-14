e302a2ca9cf253d6a05c6954a6b68b28647c7a94
SergiuMuntean <sergiu_stephan16@yahoo.com>
2023-10-14 10:50:45 +0300

    Start and end of longest sequence of 0s

---
+++ Rezolvari PbInfo/Clasa a IX-a/Tablouri unidimensionale - vectori/Interclasare/SecvZero/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void readArray(int* array, int* size)
+{
+    scanf("%d", size);
+    for(int i=0; i<*size; i++)
+        scanf("%d", array+i);
+}
+
+void findSequence(int* array, int size)
+{
+    int left = 1;
+    int right = 0;
+    for(int i=0; i<size; i++)
+        if(array[i]==0)
+        {
+            int j = i;
+            while(j+1<size && array[j+1]==0)
+                j++;
+            if(j-i+1 > right-left+1)
+            {
+                left = i;
+                right = j;
+            }
+            i = j;
+        }
+    printf("%d %d", left+1, right+1);
+}
+
+int main()
+{
+    int arr[1000], dimension;
+    readArray(arr, &dimension);
+    findSequence(arr, dimension);
+    return 0;
+}
